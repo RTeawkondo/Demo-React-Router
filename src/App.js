937afe31b221d3ade52fe1cac7d3b76bca8fd668
Peto Gurin <no.mi83@yahoo.com.vn>
2023-07-25 14:46:18 +0700

    createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Product from "./pages/Product";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage/>},
+  { path: "/product", element: <Product/>}
+])
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
