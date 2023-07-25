fac23d9c9360fc059557cb97cdb28b65fc52aa99
Peto Gurin <no.mi83@yahoo.com.vn>
2023-07-25 14:51:48 +0700

    Router the old days

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Product from "./pages/Product";
 
@@ -7,8 +7,17 @@ const router = createBrowserRouter([
   { path: "/product", element: <Product/>}
 ])
 
+const routerOlday = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage/>} />
+    <Route path="/product" element={<Product/>} />
+  </Route>
+)
+
+const routerOlderDay = createBrowserRouter(routerOlday)
+
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={routerOlderDay}/>;
 }
 
 export default App;
