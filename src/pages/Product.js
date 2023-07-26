0bce66b40d9c952d1aecaedc6bd56aa05b433025
Peto Gurin <no.mi83@yahoo.com.vn>
2023-07-26 14:26:10 +0700

    Alter way with useNavigate

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Product() {
+  const navigate = useNavigate()
+
+  function navigateHandler () {
+    navigate("/")
+  }
   return (
     <>
       <h1>hehe</h1>
       <p>Go to <Link to="/"> the home</Link></p>
+      <p>
+        <button onClick={navigateHandler}>Home back</button>
+      </p>
     </>
   );
 }
